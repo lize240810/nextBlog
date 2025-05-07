e7b1d89faecb5f208c1192e5b5c3680de40b33b1
lize <lz@qq.com>
2025-05-07 11:31:43 +0800

    Layout fix v2

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='bg-[#e7eef4] min-h-[100vh]'>
+        <div className='bg-[#e7eef4] min-h-[100vh] max-w-[1920px]'>
           <Header />
           <main>{children}</main>
         </div>
